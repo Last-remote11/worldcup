36cf184d35c18561d9510859130f6f67f4ab0a29
Sung Dong Kim <67620856+Last-remote11@users.noreply.github.com>
2021-01-27 16:01:33 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import NavigateBar from './NavigateBar'
 import {Candidates} from './Candidates'
 
 
-const getBaseLog = (x, y) => {
-  return Math.log(y) / Math.log(x);
-}
 
 
 const shuffle = (array) => {
@@ -28,37 +25,83 @@ return array;
 const App = () => {
 
   const [round, setRound] = useState(1)
-  const [numberOfCandidates, setNumberOfCandidates] = useState(16)
-  const [match, setMatch] = useState([])
+  const [numberOfCandidates, setNumberOfCandidates] = useState(4)
   const [background, setBackground] = useState('body { background-color: black; }') // 다크, 라이트모드
   const [candidates, setCandidates] = useState(shuffle(Candidates)) // 후보(무작위섞음)
-  const [currentCandidates, setCurrentCandidates] = useState([Candidates[0], Candidates[1]]) // 매칭
 
   const [wins, setWins] = useState([]) // 이긴넘들
 
-  
 
-  const onPick = (id) => {
-    console.log(id)
-    Candidates.forEach((element, i) => {
-      if (element.id === id) {
-        setWins(oldArray => [...oldArray, Candidates[i]])
-      }
-    })
-    // 동기 처리가 안돼서 오류가 일어나는것같음
-    console.log(wins)
-    if (round === numberOfCandidates / 2) { // 마지막라운드
+  let isEnd = false
+
+  let currentCandidates = [candidates[(round-1)*2], candidates[(round-1)*2+1]]
+
+
+  useEffect(() => {
+    console.log(round)
+    if (numberOfCandidates === 2) {
+      console.log(`우승 : ${{wins}}`)
+      isEnd = true
+    } else if (round === (numberOfCandidates / 2) + 1) { // 마지막라운드이면
       setCandidates(wins)
+      console.log('막라setwins', wins)
       setWins([])
-      setRound(1)
+      console.log('막라setwins2', wins)
       setNumberOfCandidates(numberOfCandidates/2)
     } else {
-      setRound(round + 1)
-      setCurrentCandidates(
-        [Candidates[(round-1)*2], Candidates[(round-1)*2+1]]
-        )
+      console.log(currentCandidates)
+      console.log(wins)
     }
+  }, [round, wins])
+
+  // useEffect(() => {
+  //   console.log('wins바뀜')
+  //   if (wins === []) {
+  //     return
+  //   } else {
+  //     if (round === numberOfCandidates / 2) {
+  //       if (numberOfCandidates === 2) {
+  //         console.log(`우승자는 ${wins} 입니다`)
+  //         isEnd = true
+  //       } else {
+  //          setRound(1)
+  //         }
+  //     } else {
+  //       setRound(round + 1)
+  //     }
+  //   }
+  // }, [wins])
+
+  // useEffect(() => {
+  //   console.log('round 바뀜')
+  //   if (round === 1) {
+  //     setCandidates(wins)
+  //     setWins([])
+  //     setNumberOfCandidates(numberOfCandidates/2)
+  //   } else {
+  //     setCurrentCandidates(
+  //       [candidates[(round-1)*2], candidates[(round-1)*2+1]]
+  //       )
+  //   }
+  // }, [round])
+
     
+
+  const onPick = (id) => {
+    console.log('pick id', id)
+    candidates.forEach((element, i) => { // 승자 추가
+      if (element.id === id) {
+        setWins(oldArray => [...oldArray, element])
+        console.log(wins)
+      }
+    })
+    if (round === numberOfCandidates / 2) {
+      setRound(1)
+      console.log('round : ',round)
+    } else {
+      setRound(round + 1)
+    }
+    console.log('wins : ', wins)
   }
 
 
--- src/CardList.js
+++ src/CardList.js
@@ -6,10 +6,8 @@ const CardList = ({ currentCandidates, onPick }) => {
 
         return(
             <div id = 'CardList'>
-                {console.log(currentCandidates)}
                 {currentCandidates.map(element =>
                      <Card key = {element.id} id = {element.id} image = {element.image} name = {element.name} onPick = {onPick}/>)}
-                <button>button</button>
             </div>
         )
     }
